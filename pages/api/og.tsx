8f7e593dcca4f5bcd8715718b955a9902c0fc445
nguyenlejoe <joe_le.1@hotmail.com>
2022-11-30 14:56:17 -0800

    fix styling

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -8,19 +8,34 @@ export const config = {
   runtime: 'experimental-edge',
 };
 
+const font = fetch(new URL('../../assets/TitilliumWeb-Regular.ttf', import.meta.url)).then(
+  (res) => res.arrayBuffer(),
+);
+
 export default async function handler(req: NextRequest) {
   const { searchParams } = req.nextUrl;
   const title = searchParams.get('title');
   const subtitle = searchParams.get('subtitle');
   const bg = searchParams.get('bg');
+  const fontData = await font;
 
   return new ImageResponse(
     ( 
-     <WebsiteTemplate title={title ? title : "One unified API One billion possibilities"} subtitle={subtitle ? subtitle : "Unified API"} bg={bg ? bg : "covalent-9"}/>
+     <WebsiteTemplate 
+        title={title ? title : <div style={{display:"flex", flexDirection: "column"}}>One unified API <br></br> One billion possibilities</div>} 
+        subtitle={subtitle ? subtitle : "Unified API"} 
+        bg={bg ? bg : "covalent-9"}/>
     ),
     {
       width: 1200,
       height: 630,
+      fonts: [
+        {
+          name: "'Titillium Web', sans-serif",
+          data: fontData,
+          style: 'normal'
+        }
+      ]
     },
   );
 }
