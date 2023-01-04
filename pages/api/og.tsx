b275edecb7026faa30b511632abd5870eb5c18bf
nguyenlejoe <joe_le.1@hotmail.com>
2023-01-04 14:57:07 -0800

    add twitter template and update main website

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -2,13 +2,15 @@ import { ImageResponse } from '@vercel/og';
 import { NextRequest } from 'next/server';
 import Image from 'next/image'
 import WebsiteTemplate from '../../templates/website';
+import TwitterTemplate from '../../templates/twitter';
+
 
 
 export const config = {
   runtime: 'experimental-edge',
 };
 
-const font = fetch(new URL('../../assets/TitilliumWeb-Regular.ttf', import.meta.url)).then(
+const font = fetch(new URL('../../assets/TitilliumWeb-Light.ttf', import.meta.url)).then(
   (res) => res.arrayBuffer(),
 );
 
@@ -17,18 +19,28 @@ export default async function handler(req: NextRequest) {
   const title = searchParams.get('title');
   const subtitle = searchParams.get('subtitle');
   const bg = searchParams.get('bg');
+  const twitter = searchParams.get('twitter');
   const fontData = await font;
 
   return new ImageResponse(
     ( 
-     <WebsiteTemplate 
-        title={title ? title : <div style={{display:"flex", flexDirection: "column"}}>One unified API <br></br> One billion possibilities</div>} 
-        subtitle={subtitle ? subtitle : "Unified API"} 
-        bg={bg ? bg : "covalent-9"}/>
+      <>
+        {!twitter ? 
+          <WebsiteTemplate 
+          title={title ? title : <div style={{display:"flex", flexDirection: "column"}}>One unified API <br></br> One billion possibilities</div>} 
+          subtitle={subtitle ? subtitle : "Unified API"} 
+          bg={bg ? bg : "covalent-14"}/>  
+          :
+          <TwitterTemplate 
+            title={title ? title : <div style={{display:"flex", flexDirection: "column"}}>One unified API <br></br> One billion possibilities</div>} 
+            subtitle={subtitle ? subtitle : "Unified API"} 
+            bg={bg ? bg : "covalent-14"}/>
+        }
+      </>
     ),
     {
-      width: 1200,
-      height: 630,
+      width: twitter ? 800 : 1200,
+      height: twitter ? 800 : 600,
       fonts: [
         {
           name: "'Titillium Web', sans-serif",
