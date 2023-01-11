2317576c9a26e215dfce0805febb6101b5cd5ba7
nguyenlejoe <joe_le.1@hotmail.com>
2023-01-11 14:07:51 -0800

    add banner template

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -3,6 +3,8 @@ import { NextRequest } from 'next/server';
 import Image from 'next/image'
 import WebsiteTemplate from '../../templates/website';
 import TwitterTemplate from '../../templates/twitter';
+import BannerTemplate from '../../templates/banner';
+
 
 
 
@@ -20,27 +22,36 @@ export default async function handler(req: NextRequest) {
   const subtitle = searchParams.get('subtitle');
   const bg = searchParams.get('bg');
   const twitter = searchParams.get('twitter');
+  const banner = searchParams.get('banner');
+
   const fontData = await font;
 
   return new ImageResponse(
     ( 
       <>
-        {!twitter ? 
+        {twitter 
+        ? 
+        <TwitterTemplate 
+        title={title ? title : <div style={{display:"flex", flexDirection: "column"}}>One unified API <br></br> One billion possibilities</div>} 
+        subtitle={subtitle ? subtitle : "Unified API"} 
+        bg={bg ? bg : "covalent-14"}/>
+        : 
+         banner ?
+          <BannerTemplate 
+            title={title ? title : <div style={{display:"flex", flexDirection: "column"}}>One unified API <br></br> One billion possibilities</div>} 
+            subtitle={subtitle ? subtitle : "Unified API"} 
+            bg={bg ? bg : "covalent-14"}/>
+          :
           <WebsiteTemplate 
           title={title ? title : <div style={{display:"flex", flexDirection: "column"}}>One unified API <br></br> One billion possibilities</div>} 
           subtitle={subtitle ? subtitle : "Unified API"} 
           bg={bg ? bg : "covalent-14"}/>  
-          :
-          <TwitterTemplate 
-            title={title ? title : <div style={{display:"flex", flexDirection: "column"}}>One unified API <br></br> One billion possibilities</div>} 
-            subtitle={subtitle ? subtitle : "Unified API"} 
-            bg={bg ? bg : "covalent-14"}/>
         }
       </>
     ),
     {
-      width: twitter ? 800 : 1200,
-      height: twitter ? 800 : 600,
+      width: twitter ? 800 : banner ? 1920 : 1200,
+      height: twitter ? 800 : banner ? 1080 : 600,
       fonts: [
         {
           name: "'Titillium Web', sans-serif",
