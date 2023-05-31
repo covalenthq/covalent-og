86291430d20c2a32b0cb24cc64c9bcf4d302e006
nguyenlejoe <joe_le.1@hotmail.com>
2023-05-31 13:56:41 -0700

    add new dato template

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -1,11 +1,9 @@
 import { ImageResponse } from '@vercel/og';
-import { NextRequest } from 'next/server';
-import Image from 'next/image'
+import { NextRequest, NextResponse } from 'next/server';
 import WebsiteTemplate from '../../templates/website';
 import TwitterTemplate from '../../templates/twitter';
 import BannerTemplate from '../../templates/banner';
-
-
+import DatoTemplate from '../../templates/dato';
 
 
 export const config = {
@@ -23,25 +21,37 @@ export default async function handler(req: NextRequest) {
   const bg = searchParams.get('bg');
   const twitter = searchParams.get('twitter');
   const banner = searchParams.get('banner');
-
+  const dato = searchParams.get('dato');
   const fontData = await font;
+  const requestHeaders = new Headers(req.headers)
+
+  const response = NextResponse.next({
+    request: {
+      headers: requestHeaders,
+    },
+  })
+  response.headers.set('Cache-Control', `public, immutable, no-transform, s-maxage=31536000, max-age=31536000`)
 
   return new ImageResponse(
     ( 
       <>
-        {twitter 
-        ? 
-        <TwitterTemplate 
-        title={title ? title : <div style={{display:"flex", flexDirection: "column"}}>One unified API <br></br> One billion possibilities</div>} 
-        subtitle={subtitle ? subtitle : "Unified API"} 
-        bg={bg ? bg : "covalent-14"}/>
-        : 
-         banner ?
+        {twitter ? 
+          <TwitterTemplate 
+          title={title ? title : <div style={{display:"flex", flexDirection: "column"}}>One unified API <br></br> One billion possibilities</div>} 
+          subtitle={subtitle ? subtitle : "Unified API"} 
+          bg={bg ? bg : "covalent-14"}/>
+        : banner ?
           <BannerTemplate 
             title={title ? title : <div style={{display:"flex", flexDirection: "column"}}>One unified API <br></br> One billion possibilities</div>} 
             subtitle={subtitle ? subtitle : "Unified API"} 
             bg={bg ? bg : "covalent-14"}/>
-          :
+        : dato ?
+          <DatoTemplate 
+          img={`https://www.datocms-assets.com/${dato}`}
+          title={title ? title : <div style={{display:"flex", flexDirection: "column"}}>One unified API <br></br> One billion possibilities</div>} 
+          subtitle={subtitle ? subtitle : "Unified API"} 
+          bg={bg ? bg : "covalent-14"}/>  
+        :
           <WebsiteTemplate 
           title={title ? title : <div style={{display:"flex", flexDirection: "column"}}>One unified API <br></br> One billion possibilities</div>} 
           subtitle={subtitle ? subtitle : "Unified API"} 
